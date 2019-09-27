116aab1c0c6275d4fa873080358664407457c30a
Jamesson Sales de Sena <jsdesena@interaxa.com>
2019-09-27 14:25:21 -0300

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //Task.Delay(1000).Wait();
+            var client = new NamedPipeClientStream("WDE");
+            try
+            {
+
+                client.Connect();
+                StreamWriter writer = new StreamWriter(client);
+
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (String.IsNullOrEmpty(input)) break;
+
+                    if (input.Equals("exit"))
+                    {
+                        client.Close();
+                        client.Dispose();
+                        Environment.Exit(1);
+                    }
+                    writer.WriteLine(input);
+                    writer.Flush();
+                }
+            }
+            catch (Exception e)
+            {
+                e = e;
+            }
+        }
+
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerNamespace
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            var server = new NamedPipeServerStream("WDE");
+            server.WaitForConnection();
+            StreamReader reader = new StreamReader(server);
+            while (true)
+            {
+                if (server.IsConnected)
+                {
+                    var line = reader.ReadLine();
+                    if (!string.IsNullOrEmpty(line))
+                        Console.WriteLine(line);
+                }
+                else
+                {
+                    server.Disconnect();
+                    server.Close();
+                    server.Dispose();
+                    server = new NamedPipeServerStream("WDE");
+                    server.WaitForConnection();
+                    reader = new StreamReader(server);
+
+
+                }
+            }
+        }
+    }
+}
